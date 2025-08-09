6fa981a291fd6b795d6e860ac7beb9fe5fb43827
thetec7 <87259083+thetec7@users.noreply.github.com>
2025-08-09 10:45:03 +0900

    Add files via upload

--- HOME.py
+++ HOME.py
@@ -12,18 +12,18 @@ st.markdown("<p style='text-align: center; font-size: 1.4em; color: #555; displa
 st.markdown("<br>", unsafe_allow_html=True)
 
 with st.container():
-    col1, col2, col3 = st.columns(3)
-    with col1:
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col2:
         if st.button("진로 직업 추천 받기", use_container_width=True):
             st.switch_page("pages/1_진로 추천 받기.py")
         if st.button("심화 탐구 주제 찾기", use_container_width=True):
             st.switch_page("pages/2_탐구 주제 찾기.py")
-    with col2:
+    with col3:
         if st.button("교과 전형 대학 찾기", use_container_width=True):
             st.switch_page("pages/3_교과 전형 대학 찾기.py")
         if st.button("종합 전형 대학 찾기", use_container_width=True):
             st.switch_page("pages/4_종합 전형 대학 찾기.py")
-    with col3:
+    with col4:
         if st.button("정시 전형 대학 찾기", use_container_width=True):
             st.switch_page("pages/5_정시 전형 대학 찾기.py")
         if st.button("특별 전형 대학 찾기", use_container_width=True):
@@ -32,7 +32,7 @@ with st.container():
 
 st.markdown("""
 <div style='text-align: center; margin-top: 50px;'>
-    <p style='color:#696969; font-size:14px;'>
+    <p style='color:#696969; f`ont-size:14px;'>
         Developed by <strong>IYT</strong><br>
         Contact: <a href="mailto:488pista@daum.net" style="color:#696969; text-decoration:none;">488pista@daum.net</a>
     </p>
